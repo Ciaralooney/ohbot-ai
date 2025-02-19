ea43199813bcdd00e6798ce531daa6f6fdc39efa
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2025-02-19 12:16:20 +0000

    Using threading so idle animation is always running until exit input

--- movement.py
+++ movement.py
@@ -1,8 +1,12 @@
+import sys
 from ohbot import ohbot
 import random
 import time
+import threading
 
 ohbot.reset()
+
+close_program = threading.Event()
 
 def move_head():
     # Head is moved in a random direction
@@ -29,7 +33,6 @@ def move_eyes():
     ohbot.move(ohbot.EYETILT, random.uniform(0, 10))
 
 def movement_sequence():
-    # Using all these functions to make a sequence
     move_head()
     ohbot.wait(1)
     blink_eyes()
@@ -46,21 +49,61 @@ def natural_head_movement():
     time.sleep(random.uniform(2, 5))
 
 def random_blink():
-    ohbot.move(ohbot.EYELIDBLINK, 0)
+    ohbot.move(ohbot.LIDBLINK, 0)
     time.sleep(0.2)
-    ohbot.move(ohbot.EYELIDBLINK, 10)
+    ohbot.move(ohbot.LIDBLINK, 10)
     # Random time until it blinks again
     time.sleep(random.uniform(3, 10))
 
-def idle_animation(seconds):
+def idle_animation_timer(seconds):
     start_time = time.time()
     while time.time() - start_time < seconds:
         natural_head_movement()
         random_blink()
+'''
+def idle_animation():
+    print("Type exit to stop movement ")
+    while True:
+        natural_head_movement()
+        random_blink()
+        user_input = input()
+        if user_input.lower() == "exit":
+            shutdown()
+            print("Ohbot has been shut down and is ready to be powered off.")
+            break
 
-if __name__ == '__main__':
-    movement_sequence()
-    # Running tape for 60 seconds
-    idle_animation(60)
+'''
+def idle_animation():
+    print("Type exit to stop movement ")
+    while not close_program.is_set():
+        natural_head_movement()
+        random_blink()
+
+def check_for_input():
+    while True:
+        user_input = input()
+        if user_input.lower() == "exit":
+            shutdown()
+            close_program.set()
+            sys.exit(0)
+
+def shutdown():
+    # Shutdown ohbot
     ohbot.reset()
     ohbot.close()
+    print("Ohbot has been shut down and is ready to be powered off.")
+
+
+if __name__ == '__main__':
+    #movement_sequence()
+    #idle_animation_timer(60)  # Running tape for 60 seconds
+
+    # Using threading so input can be taken while animations play
+    animation_thread = threading.Thread(target=idle_animation)
+    input_thread = threading.Thread(target=check_for_input)
+
+    animation_thread.start()
+    input_thread.start()
+
+    input_thread.join()
+    animation_thread.join()
